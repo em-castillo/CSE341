380edefcbccee69878cf2e367313e8475e80fd61
Emily <cas20037@byui.edu>
2023-01-06 21:09:13 -0700

    lesson01

---
+++ lesson01/controllers/index.js
@@ -0,0 +1,7 @@
+function returnData() {
+    return 'This string is coming from the controllers folder';
+};
+
+module.exports = {
+    returnData,
+  };
---
+++ lesson01/server.js
@@ -0,0 +1,12 @@
+// Create an express app that listens on either 
+// process.env.port or 3000
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'))
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`)
+})
