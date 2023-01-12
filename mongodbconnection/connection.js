7c528b9d9c09d532aae1957148d28517787770c9
Emily <cas20037@byui.edu>
2023-01-11 23:01:59 -0700

    edit

--- mongodbconnection/connection.js
+++ mongodbconnection/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = "mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.jjximnf.mongodb.net/?retryWrites=true&w=majority";
+const URI = "mongodb+srv://dbUser:dbUser@cluster0.jjximnf.mongodb.net/?retryWrites=true&w=majority";
 
 const connectDB = async() => {
     await mongoose.connect(URI);
